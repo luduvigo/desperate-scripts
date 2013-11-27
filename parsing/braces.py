d6a05db9eaf4ca8d4ae2a193607cc8dbd85be795
luduvigo <paoloantoniorossi@gmail.com>
2013-11-28 00:12:08 +0100

    Add indentation in the brace insertion in brace.py

--- parsing/braces.py
+++ parsing/braces.py
@@ -1,15 +1,15 @@
 ##########################################################################
 # Simple python script that is adding braces in if conditions of C++ files
 #
-# author: luduvigo
+# author: luduvigo (paoloantoniorossi@gmail.com)
 ##########################################################################
 
 
 import sys
 import shutil
+import string
 
 infile = sys.argv[1]
-
 
 # Backup of the file modified
 shutil.copyfile(infile, infile + '.backup')
@@ -18,23 +18,29 @@ inf = open(infile + '.backup')
 # Write file.
 out = open(infile,"w")
 skip = False
+space = ""
 for line in inf.readlines():
 	if skip == True:
 		if "{" in line: 
 			out.write(line)
 			print "-> brace already there"
 		else:
-			out.write('{\n')
-			out.write('\t' + line)
-			out.write('}\n')
+			dist = len(line) - len(line.lstrip())
+			out.write( space + '{\n' )
+			out.write( space + '\t' + line.lstrip() )
+			out.write( space + '}\n')
 			print "-> braces added"
 		skip = False
 	elif "if(" in line and line[-2] == ')':
 		out.write(line)
+		dist = len(line) - len(line.lstrip())
+		space = line[:dist]
 		print "Found: " + line[:-1]
 		skip = True
 	elif "if (" in line and line[-2] == ")" :
 		out.write(line)
+		dist = len(line) - len(line.lstrip())
+		space = line[:dist]
 		print "Found: " + line[:-1]
 		skip = True
 	else:
